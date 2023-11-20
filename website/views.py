bafd2c08f0b95744f8942b19ab66b56e6be0d814
griessmk <griessmk@oregonstate.edu>
2023-11-19 16:23:56 -0800

    test

--- website/views.py
+++ website/views.py
@@ -3,6 +3,7 @@ from flask_login import login_required, current_user
 from .models import Note
 from . import db
 import json
+import requests
 
 views = Blueprint('views', __name__)
 
@@ -26,11 +27,23 @@ def home():
 @views.route('/search')
 def search():
     search_query = request.args.get('search_query')
-    # Your search logic will go here
     results = []
     if search_query:
-        # Implement the search logic here
-        pass
+        # Use a string for the API key
+        api_key = 'AIzaSyAy9h5zz1UiXzf9FnqiIPg0P0AMk3VGjbw'  
+        params = {
+            'q': search_query,
+            'key': api_key
+        }
+        # Ensure you use the correct URL for the Google Books API
+        response = requests.get('https://www.googleapis.com/books/v1/volumes', params=params)
+        
+        if response.ok:
+            data = response.json()
+            results = data.get('items', [])
+            # Further processing of results...
+        else:
+            flash('Error fetching results from Google Books', category='error')
     return render_template("search.html", user=current_user, results=results)
 
 
